8a3fa793ce8960d802c3d196ba9a08e7755208fd
Maksym Mospanenko <mmospanenko@gmail.com>
2014-12-03 22:49:25 +0200

    Init. Tests empty

---
+++ tests.py
@@ -0,0 +1,46 @@
+import os
+import flaskr
+import unittest
+import tempfile
+
+
+class FlaskrTestCase(unittest.TestCase):
+
+	def setUp(self):
+		pass
+
+	def tearDown(self):
+		pass
+
+	def login_form_present_for_guests():
+		pass
+
+	def guest_can_create_account():
+		pass
+
+	def user_can_add_book():
+		pass
+
+	def user_can_edit_book():
+		pass
+
+	def user_can_delete_book():
+		pass
+
+	def user_can_add_author():
+		pass
+
+	def user_can_edit_author():
+		pass
+
+	def user_can_delete_author():
+		pass
+
+	def guest_can_find_the_book_by_title():
+		pass
+
+	def guest_can_find_the_book_by_author():
+		pass
+
+	def user_can_sign_out():
+		pass
