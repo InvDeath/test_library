29fec0a470402af78590157275fbca2f64de35e1
Maksym Mospanenko <mmospanenko@gmail.com>
2014-12-16 02:56:43 +0200

    Fix login view.

--- app/views.py
+++ app/views.py
@@ -44,7 +44,6 @@ def login():
 		return redirect('/')
 
 	context = {
-		'form': form,
-		'providers': app.config['OPENID_PROVIDERS']
+		'form': form
 	}
 	return render_template('login.html', **context)
