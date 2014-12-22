32581fc50a66ffa24c6e47a514adba7a1398d101
Maksym Mospanenko <mmospanenko@gmail.com>
2014-12-22 22:57:44 +0200

    Books list on frontpage.

--- app/views.py
+++ app/views.py
@@ -7,35 +7,7 @@ from .models import User, Book, Author
 
 @app.route('/')
 def index():
-    books = [
-        {
-            'title': 'book1',
-            'id': 1,
-            'author':
-            {
-                'id': 1,
-                'name': 'auth1'
-            }
-        },
-        {
-            'title': 'book2',
-            'id': 2,
-            'author':
-            {
-                'id': 1,
-                'name': 'auth1'
-            }
-        },
-        {
-            'title': 'book3',
-            'id': 3,
-            'author':
-            {
-                'id': 2,
-                'name': 'auth2'
-            }
-        },
-    ]
+    books = Book.query.all()
     return render_template('index.html', books=books)
 
 
